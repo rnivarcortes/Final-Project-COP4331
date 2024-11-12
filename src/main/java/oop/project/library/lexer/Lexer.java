94428fbefbfd735bcbed60364b92728156e6fdd4
Ricardo Nivar-Cortes <rnivarcortes@ufl.edu>
2024-11-11 22:55:02 -0500

    Checking Something

--- src/main/java/oop/project/library/lexer/Lexer.java
+++ src/main/java/oop/project/library/lexer/Lexer.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class Lexer {
 
     public static Map<String, String> lex(String input) {
+        
         if (input.length() == 0) {
             return new HashMap<>();
         }
